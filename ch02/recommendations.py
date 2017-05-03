49d27606161744538386a08aaa8a9e9c3f648445
keer2345 <keer2345@gmail.com>
2017-05-03 13:57:08 +0800

    欧几里德

--- ch02/recommendations.py
+++ ch02/recommendations.py
@@ -1,27 +1,9 @@
 # -*- coding:utf-8 -*-
 from math import sqrt
-
-critics = {'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
-                         'Just My Luck': 3.0, 'Superman Returns': 3.5, 'You, Me and Dupree': 2.5,
-                         'The Night Listener': 3.0},
-           'Gene Seymour': {'Lady in the Water': 3.0, 'Snakes on a Plane': 3.5,
-                            'Just My Luck': 1.5, 'Superman Returns': 5.0, 'The Night Listener': 3.0,
-                            'You, Me and Dupree': 3.5},
-           'Michael Phillips': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.0,
-                                'Superman Returns': 3.5, 'The Night Listener': 4.0},
-           'Claudia Puig': {'Snakes on a Plane': 3.5, 'Just My Luck': 3.0,
-                            'The Night Listener': 4.5, 'Superman Returns': 4.0,
-                            'You, Me and Dupree': 2.5},
-           'Mick LaSalle': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0,
-                            'Just My Luck': 2.0, 'Superman Returns': 3.0, 'The Night Listener': 3.0,
-                            'You, Me and Dupree': 2.0},
-           'Jack Matthews': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0,
-                             'The Night Listener': 3.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5},
-           'Toby': {'Snakes on a Plane': 4.5, 'You, Me and Dupree': 1.0, 'Superman Returns': 4.0}}
+import critics
 
 
-#  欧几里得距离
-#  Returns a distance-based similarity score for person1 and person2
+#  欧几里德距离
 def sim_distance(prefs, person1, person2):
     si = {}
     for item in prefs[person1]:
@@ -33,10 +15,14 @@ def sim_distance(prefs, person1, person2):
         return 0
 
     #  计算所有差值的平方和
+    #  sum_of_squares = sum(pow(prefs[person1][item] - prefs[person2][item], 2)
+        #  for item in prefs[person1] if item in prefs[person2])
     sum_of_squares = sum(pow(prefs[person1][item] - prefs[person2][item], 2)
-                         for item in prefs[person1] if item in prefs[person2])
+                         for item in si)
     return 1 / (1 + sqrt(sum_of_squares))
 
 
 if __name__ == '__main__':
-    print sim_distance(critics, 'Lisa Rose', 'Gene Seymour')
+    print sim_distance(critics.critics, 'Lisa Rose', 'Gene Seymour')
+    #  0.294298055086
+    print sim_distance(critics.critics, 'person1', 'person2')
